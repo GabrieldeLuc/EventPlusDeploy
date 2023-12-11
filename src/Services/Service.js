6eb0442b54e7d119e014d1b58821b91cff537648
Gabriel Lucca <gabriel.santana60@aluno.senai.br>
2023-12-11 15:07:04 -0300

    Alterado String de Conexão API Azure

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -40,14 +40,16 @@ export const institutionResource = '/Instituicao';
  */
 export const loginResource = '/Login';
 
-const apiPort = '5000';
-const localApiUri = `http://localhost:${apiPort}/api`;
-const externallApiUri = ``;
+//const apiPort = '5000';
+//const localApiUri = `http://localhost:${apiPort}/api`;
+const externallApiUri = `https://webapigabriel.azure-api.net/api`;
 // const externalApiUri = null;
 
 const api = axios.create({
-    baseURL: localApiUri
+    baseURL: externallApiUri
 });
+
+
 
 
 
